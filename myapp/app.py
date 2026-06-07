81f478c484c66c64bf6793668a8b4b781e11a5f4
Waqas Khalique <waqcas@gmail.com>
2026-06-07 12:31:54 +0500

    updated

--- myapp/app.py
+++
@@ -1,9 +0,0 @@
-from flask import  Flask
-
-app = Flask(__name__)
-
-@app.route('/healthz', methods=['GET'])
-def about():
-    message = "healthy"
-    return {'message': message}, 200
-#gunicorn --bind localhost:8888 app:app
--- terraform/0-locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  env         = "prod"
-  region      = "ap-south-1"
-  zone1       = "ap-south-1a"
-  zone2       = "ap-south-1b"
-  eks_name    = "demo"
-  eks_version = "1.30"
-}
